82db13575cdee5eff0aa38ca117fe6927cfface9
UCPolisen <adrian.neshad1@gmail.com>
2025-05-04 12:00:02 +0200

    Initial Commit

---
+++ Univert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Univert
+//
+//  Created by Adrian Neshad on 2025-05-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Univert/UnivertApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnivertApp.swift
+//  Univert
+//
+//  Created by Adrian Neshad on 2025-05-04.
+//
+
+import SwiftUI
+
+@main
+struct UnivertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
