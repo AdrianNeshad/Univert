d6dcb5d3820c7f21a5eb8f067465e4414eac4f1f
UCPolisen <adrian.neshad1@gmail.com>
2025-05-05 22:22:27 +0200

    a

--- Univert/Units/Length.swift
+++ Univert/Units/Length.swift
@@ -4,23 +4,172 @@
 //
 //  Created by Adrian Neshad on 2025-05-04.
 //
-
 import SwiftUI
 
 struct Längd: View {
+    @State private var selectedFromUnit: String? = "m"
+    @State private var selectedToUnit: String? = "m"
+    @State private var inputValue = ""
+    @State private var outputValue = ""
+    
+    let units = ["m", "cm", "km", "dm", "mm", "mi", "in", "ft", "yd", "µm", "nm"]
+    
     var body: some View {
         VStack {
-            Text("📐")
-                .font(.system(size: 100))
-            Text("This is the Length page")
-                .font(.title)
-        }
-        .navigationTitle("Längd")
-    }
-}
+            HStack {
+                Text("Från")
+                    .font(.title)
+                    .bold()
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding(10)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(5)
+                    .multilineTextAlignment(.center)
+                
+                Text("➤")
+                    .font(.title)
+                    .bold()
+                    .frame(width: 100)
+                    .padding(.leading, 10)
+                    .padding(.trailing, 10)
 
-struct Längd_Previews: PreviewProvider {
-    static var previews: some View {
-        Längd()
+                Text("Till")
+                    .font(.title)
+                    .bold()
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding(10)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(5)
+                    .multilineTextAlignment(.center)
+            } //HStack
+            
+            HStack {
+                Text("►")
+                    .font(.title)
+                    .frame(width: 50)
+                PomodoroPicker(
+                    selection: $selectedFromUnit,
+                    options: units
+                ) { unit in
+                    Text(unit)
+                        .font(.title)
+                        .bold()
+                        .frame(width: 100)
+                        .padding(.leading, -90)
+                }
+                PomodoroPicker(
+                    selection: $selectedToUnit,
+                    options: units
+                ) { unit in
+                    Text(unit)
+                        .font(.title)
+                        .bold()
+                        .frame(width: 100)
+                        .padding(.trailing, -90)
+                }
+                Text("◄")
+                    .font(.title)
+                    .frame(width: 50)
+            } //HStack
+            .frame(maxWidth: .infinity)
+            .frame(height: 180)
+            
+            HStack {
+                Text("(\(selectedFromUnit ?? ""))")
+                    .font(.system(size: 15))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 10)
+                
+                Text("(\(selectedToUnit ?? ""))")
+                    .font(.system(size: 15))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 0)
+            } //HStack
+            
+            HStack(spacing: 10) {
+                TextField("Värde", text: $inputValue)
+                    .keyboardType(.decimalPad)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding(10)
+                    .frame(height: 50)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(5)
+                    .multilineTextAlignment(.leading)
+                    .onChange(of: inputValue) { newValue in
+                        // Omvandla komma till punkt och försök att konvertera till Double
+                        let formattedValue = newValue.replacingOccurrences(of: ",", with: ".")
+                        if let inputDouble = Double(formattedValue) {
+                            updateOutputValue(inputDouble: inputDouble)
+                        } else {
+                            outputValue = ""
+                        }
+                    }
+                    .onChange(of: selectedFromUnit) { _ in
+                        // Uppdatera output när enheten ändras
+                        if let inputDouble = Double(inputValue) {
+                            updateOutputValue(inputDouble: inputDouble)
+                        }
+                    }
+                    .onChange(of: selectedToUnit) { _ in
+                        // Uppdatera output när enheten ändras
+                        if let inputDouble = Double(inputValue) {
+                            updateOutputValue(inputDouble: inputDouble)
+                        }
+                    }
+
+                Text(outputValue.isEmpty ? "" : outputValue)
+                    .padding(10)
+                    .frame(height: 50)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(5)
+                    .multilineTextAlignment(.leading)
+            } //HStack
+            .padding([.leading, .trailing], 10)
+        } //VStack
+        .padding(.top, 20)
+        Spacer()
+        .navigationTitle("Längd")
+        .padding()
+    }
+    
+    func convertLength(value: Double, fromUnit: String, toUnit: String) -> Double? {
+        let conversionFactors: [String: Double] = [
+            "m": 1, // basenhet
+            "cm": 0.01, // centimeter till meter
+            "km": 1000, // kilometer till meter
+            "dm": 0.1, // decimeter till meter
+            "mm": 0.001, // millimeter till meter
+            "mi": 1609.34, // miles till meter
+            "in": 0.0254, // inch till meter
+            "ft": 0.3048, // fot till meter
+            "yd": 0.9144, // yard till meter
+            "µm": 0.000001, // mikrometer till meter
+            "nm": 0.000000001 // nanometer till meter
+        ]
+        
+        // Kontrollera att enheterna finns i conversionFactors
+        guard let fromFactor = conversionFactors[fromUnit], let toFactor = conversionFactors[toUnit] else {
+            return nil // Om någon enhet inte finns i listan, returnera nil
+        }
+
+        // Omvandla till meter (basenhet)
+        let valueInMeters = value * fromFactor
+        
+        // Omvandla från meter till mål-enhet
+        let convertedValue = valueInMeters / toFactor
+        return convertedValue
+    }
+
+    func updateOutputValue(inputDouble: Double) {
+        if let result = convertLength(value: inputDouble, fromUnit: selectedFromUnit ?? "", toUnit: selectedToUnit ?? "") {
+            let formattedResult = String(format: "%.2f", result).replacingOccurrences(of: ".", with: ",")
+            outputValue = formattedResult
+        } else {
+            outputValue = "Ogiltig enhet"
+        }
     }
 }
--- Univert/Units/Time.swift
+++ Univert/Units/Time.swift
@@ -8,19 +8,166 @@
 import SwiftUI
 
 struct Tid: View {
+    @State private var selectedFromUnit: String? = "ms"
+    @State private var selectedToUnit: String? = "ms"
+    @State private var inputValue = ""
+    @State private var outputValue = ""
+    
+    let units = ["ms", "s", "min", "h", "dag", "vecka", "månad", "år"]
+    
     var body: some View {
         VStack {
-            Text("⏰")
-                .font(.system(size: 100))
-            Text("This is the Time page")
-                .font(.title)
-        }
-        .navigationTitle("Tid")
-    }
-}
+            HStack {
+                Text("Från")
+                    .font(.title)
+                    .bold()
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding(10)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(5)
+                    .multilineTextAlignment(.center)
+                
+                Text("➤")
+                    .font(.title)
+                    .bold()
+                    .frame(width: 100)
+                    .padding(.leading, 10)
+                    .padding(.trailing, 10)
 
-struct Tid_Previews: PreviewProvider {
-    static var previews: some View {
-        Tid()
+                Text("Till")
+                    .font(.title)
+                    .bold()
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding(10)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(5)
+                    .multilineTextAlignment(.center)
+            } //HStack
+            
+            HStack {
+                Text("►")
+                    .font(.title)
+                    .frame(width: 50)
+                PomodoroPicker(
+                    selection: $selectedFromUnit,
+                    options: units
+                ) { unit in
+                    Text(unit)
+                        .font(.title)
+                        .bold()
+                        .frame(width: 100)
+                        .padding(.leading, -90)
+                }
+                PomodoroPicker(
+                    selection: $selectedToUnit,
+                    options: units
+                ) { unit in
+                    Text(unit)
+                        .font(.title)
+                        .bold()
+                        .frame(width: 100)
+                        .padding(.trailing, -90)
+                }
+                Text("◄")
+                    .font(.title)
+                    .frame(width: 50)
+            } //HStack
+            .frame(maxWidth: .infinity)
+            .frame(height: 180)
+            
+            HStack {
+                Text("(\(selectedFromUnit ?? ""))")
+                    .font(.system(size: 15))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 10)
+                
+                Text("(\(selectedToUnit ?? ""))")
+                    .font(.system(size: 15))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 0)
+            } //HStack
+            
+            HStack(spacing: 10) {
+                TextField("Värde", text: $inputValue)
+                    .keyboardType(.decimalPad)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding(10)
+                    .frame(height: 50)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(5)
+                    .multilineTextAlignment(.leading)
+                    .onChange(of: inputValue) { newValue in
+                        // Omvandla komma till punkt och försök att konvertera till Double
+                        let formattedValue = newValue.replacingOccurrences(of: ",", with: ".")
+                        if let inputDouble = Double(formattedValue) {
+                            updateOutputValue(inputDouble: inputDouble)
+                        } else {
+                            outputValue = ""
+                        }
+                    }
+                    .onChange(of: selectedFromUnit) { _ in
+                        // Uppdatera output när enheten ändras
+                        if let inputDouble = Double(inputValue) {
+                            updateOutputValue(inputDouble: inputDouble)
+                        }
+                    }
+                    .onChange(of: selectedToUnit) { _ in
+                        // Uppdatera output när enheten ändras
+                        if let inputDouble = Double(inputValue) {
+                            updateOutputValue(inputDouble: inputDouble)
+                        }
+                    }
+
+                Text(outputValue.isEmpty ? "" : outputValue)
+                    .padding(10)
+                    .frame(height: 50)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(5)
+                    .multilineTextAlignment(.leading)
+            } //HStack
+            .padding([.leading, .trailing], 10)
+        } //VStack
+        .padding(.top, 20)
+        Spacer()
+        .navigationTitle("Tid")
+        .padding()
+    }
+    
+    func convertTime(value: Double, fromUnit: String, toUnit: String) -> Double? {
+        let conversionFactors: [String: Double] = [
+            "s": 1, // basenhet (sekunder)
+            "ms": 0.001, // millisekunder till sekunder
+            "min": 60, // minuter till sekunder
+            "h": 3600, // timmar till sekunder
+            "dag": 86400, // dagar till sekunder
+            "vecka": 604800, // veckor till sekunder
+            "månad": 2592000, // månad (genomsnittlig månad) till sekunder
+            "år": 31536000 // år till sekunder (genomsnittligt år)
+        ]
+        
+        // Kontrollera att enheterna finns i conversionFactors
+        guard let fromFactor = conversionFactors[fromUnit], let toFactor = conversionFactors[toUnit] else {
+            return nil // Om någon enhet inte finns i listan, returnera nil
+        }
+
+        // Omvandla till sekunder (basenhet)
+        let valueInSeconds = value * fromFactor
+        
+        // Omvandla från sekunder till mål-enhet
+        let convertedValue = valueInSeconds / toFactor
+        return convertedValue
+    }
+
+    func updateOutputValue(inputDouble: Double) {
+        if let result = convertTime(value: inputDouble, fromUnit: selectedFromUnit ?? "", toUnit: selectedToUnit ?? "") {
+            let formattedResult = String(format: "%.2f", result).replacingOccurrences(of: ".", with: ",")
+            outputValue = formattedResult
+        } else {
+            outputValue = "Ogiltig enhet"
+        }
     }
 }
