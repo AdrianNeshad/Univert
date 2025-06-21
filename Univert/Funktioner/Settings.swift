d780ba2e76f27bb682c1d81b2e308529b4ea6656
Adrian <adrian.neshad1@gmail.com>
2025-06-21 18:25:28 +0200

    .

--- Univert/Funktioner/Settings.swift
+++ Univert/Funktioner/Settings.swift
@@ -207,6 +207,15 @@ struct Inställningar: View {
                         Text("FlixSwipe - Explore New Movies")
                     }
                 }
+                Link(destination: URL(string: "https://apps.apple.com/us/app/unifeed-nyhetsfl%C3%B6de/id6746872036")!) {
+                    HStack {
+                        Image("unifeed")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .cornerRadius(8)
+                        Text("Unifeed - Nyhetsflöde")
+                    }
+                }
             }
             
             Section {
